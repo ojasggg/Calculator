824a50b5bd387731e0f0f0364afb15ad642b96f0
Ojash Gurung <ojasggg@gmail.com>
2021-06-20 20:34:21 +0545

    Completed with only two numbers and without exceptions.

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,119 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvOutput : TextView
+    private lateinit var tvInput : TextView
+    private lateinit var btn0 : Button
+    private lateinit var btn1 : Button
+    private lateinit var btn2 : Button
+    private lateinit var btn3 : Button
+    private lateinit var btn4 : Button
+    private lateinit var btn5 : Button
+    private lateinit var btn6 : Button
+    private lateinit var btn7 : Button
+    private lateinit var btn8 : Button
+    private lateinit var btn9 : Button
+    private lateinit var btnAdd : Button
+    private lateinit var btnSub : Button
+    private lateinit var btnDivide : Button
+    private lateinit var btnDec : Button
+    private lateinit var btnMul : Button
+    private lateinit var btnEqual : Button
+    private lateinit var btnClear : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvOutput = findViewById(R.id.tvOutput)
+        tvInput = findViewById(R.id.tvInput)
+        btn0 = findViewById(R.id.btn0)
+        btn1 = findViewById(R.id.btn1)
+        btn2 = findViewById(R.id.btn2)
+        btn3 = findViewById(R.id.btn3)
+        btn4 = findViewById(R.id.btn4)
+        btn5 = findViewById(R.id.btn5)
+        btn6 = findViewById(R.id.btn6)
+        btn7 = findViewById(R.id.btn7)
+        btn8 = findViewById(R.id.btn8)
+        btn9 = findViewById(R.id.btn9)
+        btnAdd = findViewById(R.id.btnAdd)
+        btnSub = findViewById(R.id.btnSub)
+        btnDivide = findViewById(R.id.btnDivide)
+        btnDec = findViewById(R.id.btnDec)
+        btnMul = findViewById(R.id.btnMul)
+        btnEqual = findViewById(R.id.btnEqual)
+        btnClear = findViewById(R.id.btnClear)
+
+        //Number listeners
+        btn0.setOnClickListener { appendOnClick(true, "0") }
+        btn1.setOnClickListener { appendOnClick(true, "1") }
+        btn2.setOnClickListener { appendOnClick(true, "2") }
+        btn3.setOnClickListener { appendOnClick(true, "3") }
+        btn4.setOnClickListener { appendOnClick(true, "4") }
+        btn5.setOnClickListener { appendOnClick(true, "5") }
+        btn6.setOnClickListener { appendOnClick(true, "6") }
+        btn7.setOnClickListener { appendOnClick(true, "7") }
+        btn8.setOnClickListener { appendOnClick(true, "8") }
+        btn9.setOnClickListener { appendOnClick(true, "9") }
+        btnDec.setOnClickListener { appendOnClick(true, ".") }
+
+
+        //Operator Listeners
+        btnAdd.setOnClickListener { appendOnClick(false, "+") }
+        btnSub.setOnClickListener { appendOnClick(false, "-") }
+        btnMul.setOnClickListener { appendOnClick(false, "*") }
+        btnDivide.setOnClickListener { appendOnClick(false, "/") }
+
+
+        btnClear.setOnClickListener {
+            clear()
+        }
+
+        btnEqual.setOnClickListener {
+            calculate()
+        }
+
+
+    }
+
+    //now create methods
+
+    private fun appendOnClick(clear: Boolean, string: String) {
+
+        if (clear) {
+            tvOutput.text = ""
+            tvInput.append(string)
+        } else {
+            tvInput.append(tvOutput.text)
+            tvInput.append(string)
+            tvOutput.text = ""
+        }
+    }
+
+    private fun clear() {
+        tvInput.text = ""
+        tvOutput.text = ""
+
+    }
+
+    private fun calculate() {
+
+        val input = ExpressionBuilder(tvInput.text.toString()).build()
+        val output = input.evaluate()
+        val longOutput = output.toLong()
+        if (output == longOutput.toDouble()){
+            tvOutput.text = longOutput.toString()
+        }else{
+            tvOutput.text = output.toString()
+        }
     }
 }
